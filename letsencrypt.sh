c775cb43053cb88ab7acbc6ea3978c6928e6f3a8
Robin Janssens <robin@robinjanssens.net>
2017-01-31 00:41:58 +0000

    added script

---
+++ letsencrypt.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+logfile="/var/letsencryptrenewal.log"
+if [ ! -f $logfile ]; then
+    touch $logfile
+fi
+echo "Date: "$(date) >> $logfile
+/opt/letsencrypt/certbot-auto renew --pre-hook "service apache2 stop" --post-hook "service apache2 start" >> $logfile
+echo "                              " >> $logfile
+echo "==============================" >> $logfile
+echo "                              " >> $logfile
